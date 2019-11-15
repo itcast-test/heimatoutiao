56ac6281d09a9cf2a4ee909125ed6d485650e6f7
lipengzhou <lpzmail@163.com>
2019-11-15 12:39:00 +0800

    用户退出

--- src/components/layout-header.vue
+++ src/components/layout-header.vue
@@ -17,7 +17,11 @@
         <el-dropdown-menu slot="dropdown">
           <el-dropdown-item>账户信息</el-dropdown-item>
           <el-dropdown-item>git地址</el-dropdown-item>
-          <el-dropdown-item>退出</el-dropdown-item>
+          <!--
+            如果想要给一个组件注册一个原生 JavaScript 事件
+            使用 .native 修饰符
+           -->
+          <el-dropdown-item @click.native="onLogout">退出</el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
     </el-col>
@@ -25,7 +29,33 @@
 </template>
 <script>
 export default {
+  methods: {
+    onLogout () {
+      this.$confirm('确认推出吗？', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // 确认执行这里
+        // 删除 token
+        window.localStorage.removeItem('user-token')
 
+        // 跳转到登录页
+        this.$router.push('/login')
+
+        this.$message({
+          type: 'success',
+          message: '退出成功!'
+        })
+      }).catch(() => {
+        // 取消执行这里
+        this.$message({
+          type: 'info',
+          message: '已取消退出'
+        })
+      })
+    }
+  }
 }
 
 </script>
