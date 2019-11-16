a3593f6135cb6b11f5d0f1a1cb0b6860e7fa654c
lipengzhou <lpzmail@163.com>
2019-11-16 15:28:37 +0800

    发布文章-准备

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,12 +1,58 @@
 <template>
-  <div class="publish">发布文章</div>
+  <div class="publish">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>发布文章</span>
+      </div>
+      <el-form ref="form" :model="article" label-width="80px">
+        <el-form-item label="标题">
+          <el-input v-model="article.title"></el-input>
+        </el-form-item>
+        <el-form-item label="内容">
+          <el-input type="textarea" v-model="article.content"></el-input>
+        </el-form-item>
+        <el-form-item label="频道">
+          <el-select v-model="article.channel_id" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <!-- <el-form-item label="封面">
+          <el-radio-group v-model="form.resource">
+            <el-radio label="线上品牌商赞助"></el-radio>
+            <el-radio label="线下场地免费"></el-radio>
+          </el-radio-group>
+        </el-form-item> -->
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">发表</el-button>
+          <el-button>存入草稿</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: '',
+  name: 'PublishArticle',
   data () {
-    return {}
+    return {
+      article: {
+        title: '', // 文章标题
+        content: '', // 文章内容
+        cover: {
+          type: 0, // 封面类型 -1:自动，0-无图，1-1张，3-3张
+          images: [] // 图片，无图就是空数组即可
+        },
+        channel_id: ''
+      }
+    }
+  },
+
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
   }
 }
 </script>
