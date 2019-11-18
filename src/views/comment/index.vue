25e71ab1f206ee34355f5e5184fc48b2f93cf2eb
lipengzhou <lpzmail@163.com>
2019-11-18 16:26:03 +0800

    评论管理-展示评论列表

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,5 +1,46 @@
 <template>
-  <div>评论组件</div>
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>评论管理</span>
+      </div>
+      <el-table
+        :data="articles"
+        style="width: 100%">
+        <el-table-column
+          prop="title"
+          label="标题"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          label="评论状态"
+          width="180">
+          <template slot-scope="scope">
+            <!-- 开关组件绑定的数据是一个布尔值，它会根据布尔值的真假来决定开关状态 -->
+            <el-switch
+              v-model="scope.row.comment_status"
+              active-color="#13ce66"
+              inactive-color="#ff4949">
+            </el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="total_comment_count"
+          label="总评论数">
+        </el-table-column>
+        <el-table-column
+          prop="fans_comment_count"
+          label="粉丝评论数据">
+        </el-table-column>
+        <el-table-column
+          label="操作">
+          <template>
+            <el-button type="primary">修改</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -10,13 +51,32 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      articles: [] // 文章列表（文章的评论数据字段）
+    }
   },
   computed: {},
   watch: {
   },
-  created () {},
-  methods: {}
+  created () {
+    this.loadArticles()
+  },
+  methods: {
+    loadArticles () {
+      this.$axios({
+        method: 'GET',
+        url: '/articles',
+        params: {
+          response_type: 'comment'
+          // page: xxx // 页码
+        }
+      }).then(res => {
+        this.articles = res.data.data.results
+      }).catch(err => {
+        console.log(err, '获取数据失败')
+      })
+    }
+  }
 }
 </script>
 
