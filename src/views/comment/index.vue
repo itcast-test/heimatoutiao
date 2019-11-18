60666f738266ae099f94b8facdc9e89c800bfeff
lipengzhou <lpzmail@163.com>
2019-11-18 16:37:07 +0800

    评论管理-修改评论状态

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -13,6 +13,14 @@
           width="180">
         </el-table-column>
         <el-table-column
+          prop="total_comment_count"
+          label="总评论数">
+        </el-table-column>
+        <el-table-column
+          prop="fans_comment_count"
+          label="粉丝评论数据">
+        </el-table-column>
+        <el-table-column
           label="评论状态"
           width="180">
           <template slot-scope="scope">
@@ -20,17 +28,10 @@
             <el-switch
               v-model="scope.row.comment_status"
               active-color="#13ce66"
-              inactive-color="#ff4949">
+              inactive-color="#ff4949"
+              @change="onStatusChange(scope.row)">
             </el-switch>
           </template>
-        </el-table-column>
-        <el-table-column
-          prop="total_comment_count"
-          label="总评论数">
-        </el-table-column>
-        <el-table-column
-          prop="fans_comment_count"
-          label="粉丝评论数据">
         </el-table-column>
         <el-table-column
           label="操作">
@@ -74,6 +75,30 @@ export default {
         this.articles = res.data.data.results
       }).catch(err => {
         console.log(err, '获取数据失败')
+      })
+    },
+
+    onStatusChange (article) {
+      this.$axios({
+        method: 'PUT',
+        url: '/comments/status',
+        params: {
+          article_id: article.id.toString()
+        },
+        data: {
+          // 开关组件双向绑定了 article.comment_status
+          // 所以获取 article.comment_status 也就是在获取开关组件的启用状态
+          allow_comment: article.comment_status
+        }
+      }).then(res => {
+        console.log(res)
+        this.$message({
+          type: 'success',
+          message: `${article.comment_status ? '启用' : '关闭'}成功`
+        })
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('操作失败')
       })
     }
   }
