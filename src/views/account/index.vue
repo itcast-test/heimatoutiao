48400f7d05d22ba18603e5a99ac9508eace79285
lipengzhou <lpzmail@163.com>
2019-11-19 16:38:27 +0800

    账户信息-展示用户信息

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,23 +1,26 @@
 <template>
   <div>
-    <el-form ref="form" :model="form" label-width="80px">
+    <el-form ref="form" :model="user" label-width="80px">
       <el-form-item label="用户头像">
         <el-upload
           class="avatar-uploader"
           action="https://jsonplaceholder.typicode.com/posts/"
           :show-file-list="false">
-          <img width="100" src="http://toutiao.meiduo.site/FuLZH0vzKbdXi-On88NArXegOjXk" class="avatar">
+          <img width="100" :src="user.photo" class="avatar">
           <!-- <i class="el-icon-plus avatar-uploader-icon"></i> -->
         </el-upload>
       </el-form-item>
       <el-form-item label="用户昵称">
-        <el-input v-model="form.name"></el-input>
+        <el-input v-model="user.name"></el-input>
       </el-form-item>
       <el-form-item label="个人介绍">
-        <el-input type="textarea" v-model="form.name"></el-input>
+        <el-input type="textarea" v-model="user.intro"></el-input>
       </el-form-item>
       <el-form-item label="用户邮箱">
-        <el-input v-model="form.name"></el-input>
+        <el-input v-model="user.email"></el-input>
+      </el-form-item>
+      <el-form-item label="手机号">
+        <el-input v-model="user.mobile"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">保存修改</el-button>
@@ -33,25 +36,38 @@ export default {
   props: {},
   data () {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+      user: {
+        email: '', // 邮箱
+        intro: '', // 简介
+        mobile: '', // 手机号
+        name: '', // 昵称
+        photo: '' // 头像
       }
     }
   },
   computed: {},
   watch: {
   },
-  created () {},
+  created () {
+    this.loadUserProfile()
+  },
   methods: {
     onSubmit () {
       console.log('保存')
+    },
+
+    loadUserProfile () {
+      this.$axios({
+        method: 'GET',
+        url: '/user/profile'
+      }).then(res => {
+        // res.date undefined
+        // undefined.data 报错了
+        this.user = res.data.data
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('获取数据失败')
+      })
     }
   }
 }
