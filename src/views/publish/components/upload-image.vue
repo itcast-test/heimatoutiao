b0d1f534017a62358c2f44d5d332c09f93f12652
lipengzhou <lpzmail@163.com>
2019-11-21 15:29:41 +0800

    封装上传封面图片组件-使用对话框

--- src/views/publish/components/upload-image.vue
+++ src/views/publish/components/upload-image.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>上传图片组件</div>
+  <div>
+    <p @click="centerDialogVisible=true">图片上传组件</p>
+    <!--
+      visible 控制对话框的显示和隐藏
+     -->
+    <el-dialog
+      title="请选择文章封面图片"
+      :visible.sync="centerDialogVisible"
+      width="30%"
+      center>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="centerDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -8,7 +23,9 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      centerDialogVisible: false
+    }
   },
   computed: {},
   watch: {
